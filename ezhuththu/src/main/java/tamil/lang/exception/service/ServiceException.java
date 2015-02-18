d1a88ff16cea232b749e8a8782afea74b9219a2c
velmurugan.subramanian <velmurugan.subramanian@oracle.com>
2015-02-17 17:56:25 -0800

    Refactoring .. ezhuththu

---
+++ ezhuththu/src/main/java/tamil/lang/exception/service/ServiceException.java
@@ -0,0 +1,15 @@
+package tamil.lang.exception.service;
+
+import tamil.lang.exception.TamilPlatformException;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author velsubra
+ */
+public class ServiceException extends TamilPlatformException {
+    public ServiceException(String msg) {
+        super(msg);
+    }
+}
---
+++ ezhuththu/src/main/java/tamil/lang/exception/service/ServiceNotAvailableException.java
@@ -0,0 +1,13 @@
+package tamil.lang.exception.service;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author velsubra
+ */
+public class ServiceNotAvailableException extends ServiceException {
+    public ServiceNotAvailableException(String msg) {
+        super(msg);
+    }
+}
